447349599233d98ffdba43dc4de25c5fbcdc4e79
Gabriel Niziolek <ganiziolek@gmail.com>
2023-01-16 14:49:46 -0300

    raise OrdemNaoEncontrada

--- FoccoERPy/exceptions/__init__.py
+++ FoccoERPy/exceptions/__init__.py
@@ -5,3 +5,10 @@ class ErroFocco(Exception):
             Erro utilizado quando houve algum erro no retorno da API
         """
         super().__init__(*args)
+
+class OrdemNaoEncontrada(Exception):
+    def __init__(self, *args: object) -> None:
+        """
+            Ordem não encontrada
+        """
+        super().__init__("Ordem não encontrada")
